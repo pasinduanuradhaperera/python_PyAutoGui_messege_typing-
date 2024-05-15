d7c3ee7743279861ee63b69eb780f229f7b6101c
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2024-05-15 16:56:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # pyautogui library
-# developed by Paecho
 
 import pyautogui as gui
-for i in range(100):
-    gui.write("your Messege Here !!!!")
+import time
+print("started you have 10 seconds to navigate") 
+for i in range(10):
+    print(i)
+    time.sleep(1)
+num = 1000
+for i in range(num):
+    gui.write("huuuuuuu")
     gui.press("enter")
+    print(num - i, "to print")
